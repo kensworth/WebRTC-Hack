706e414733190d33341a9c837a88691a41eab56b
Ken Zhang <kyz211@nyu.edu>
2016-08-01 00:07:56 -0400

    initial commit - add server, use babel/webpack, pull in twilio env vars

---
+++ server.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import express from 'express';
+import http from 'http';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import path from 'path';
+import io from 'socket.io';
+import twilio from 'twilio';
+import env from 'node-env-file';
+
+env('./.env');
+const APP_PORT = 3002;
+const acccountSID = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(acccountSID, authToken);
+const app = express();
+const server = http.createServer(app);
+const ios = io.listen(server);
+const compiler = webpack({
+  entry: path.resolve(__dirname, 'js', 'app.js'),
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        test: /\.js$/,
+      },
+    ],
+  },
+  output: {filename: 'app.js', path: '/'},
+});
+const socketOpen = (err, token) => {
+	ios.sockets.on('connection', function(socket) {
+		const log = () => {
+			let array = ['>>> '];
+			for(let i = 0; i < arguments.length; i++) {
+				array.push(arguments[i]);
+			}
+			socket.emit('log', array);
+		}
+		socket.on('message', message => {
+			log('Got message: ', message);
+			// fix for room only. pass in room
+			socket.broadcast.emit('message', message);
+		})
+	});
+}
+
+app.use(webpackMiddleware(compiler, {
+	noInfo: false,
+	publicPath: '/'
+}));
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+server.listen(APP_PORT, () => {
+  console.log(`App is now running on http://localhost:${APP_PORT}`);
+});
+client.tokens.create({}, (err, token) => {
+	socketOpen(err, token);
+});
